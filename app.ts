8c1d83a27d33e68cb8b6a25bf48484ab36f851a0
NaiKit <kitkatkung2545@gmail.com>
2024-02-29 14:27:32 +0700

    first-commit

---
+++ app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+export const app = express();
+app.use("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Node Express start!!!");
+});
